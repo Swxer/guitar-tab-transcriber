d2752e1d8b413c7863a9c61705623ff28914001c
Swxer <swxergamer64@gmail.com>
2025-08-01 23:56:20 +1000

    implemented load audio function and get pitch from the audio

--- src/main.py
+++ src/main.py
@@ -1,7 +1,26 @@
 import librosa
-import numpy
-import sounddevice
-import scipy
-import pydub
+import numpy as np
+import sounddevice as sd
+# import scipy
+# import pydub
 
-print('hello world')
+SAMPLE_RATE = 44100
+
+def load_audio_file(filename):
+    return librosa.load(filename)
+
+def get_pitch(y,sr):
+    
+    fmin = librosa.note_to_hz('E2')
+    fmax = librosa.note_to_hz('C7')
+    
+    f0, voiced_flag, voiced_prob = librosa.pyin(y,fmin=fmin,fmax=fmax, sr=sr)
+    return f0
+
+
+if __name__ == '__main__':
+    print('hello world')
+    y, sr = load_audio_file('./data/A2.wav')
+
+    f0 = get_pitch(y,sr)
+    print(f'Detected pitch: {f0}')
