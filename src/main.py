9f1e507cb5237034101b8095f795305c52cc98d3
Swxer <swxergamer64@gmail.com>
2025-08-05 18:48:52 +1000

    detect multiple notes from a single audio file

--- src/main.py
+++ src/main.py
@@ -1,52 +1,15 @@
 import librosa
 import numpy as np
 import sounddevice as sd
-from utils import visualize
+from utils import visualize, load_audio_file, get_pitch, get_detected_notes, onset_detect
 # import scipy
 # import pydub
 
-SAMPLE_RATE = 44100
-
-guitar_notes = {
-    'E2': 82.41,
-    'A2': 110,
-    'D3': 146.83,
-    'G3': 196,
-    'B3': 246.94,
-    'E4': 329.63
-}
-
-expected_ranges = {
-    'E2': (75, 90),
-    'A2': (100, 120),
-    'D3': (140, 160),
-    'G3': (190, 210),
-    'B3': (240, 260),
-    'E4': (320, 340)
-}
-
-
-def load_audio_file(filename):
-    return librosa.load(filename)
-
-def get_pitch(y,sr):
-    
-    fmin = librosa.note_to_hz('E2')
-    fmax = librosa.note_to_hz('C7')
-    
-    f0, voiced_flag, voiced_prob = librosa.pyin(y, fmin=fmin, fmax=fmax, sr=sr)
-    return f0, voiced_flag
-
-def onset_detect(y,sr):
-    return librosa.onset.onset_detect(y=y, sr=sr, units='time')
-
-
 if __name__ == '__main__':
     print('hello world')
-
     # sr - sample rate
     # y - numpy array that represents amplitude of sound wave at each sample point
-    y, sr = load_audio_file('./data/A2.wav')
+    y, sr = load_audio_file('./data/twinkle.wav')
 
     # f0 - fundamental frequency, an array that contains estimated funamental pitch for each frame of the audio
     # voiced_flag is a boolean array that corresponds one-to-one with f0, 
@@ -55,30 +18,11 @@ if __name__ == '__main__':
     f0, voiced_flag = get_pitch(y,sr)
     onsets_frame = onset_detect(y,sr)
 
-    print(f'Detected pitch: {f0}')
     print(f'Detected onsets (in seconds): {onsets_frame}')
 
-
-    # create a new array with only the pitches we are confident about 
-    confident_f0 = f0[voiced_flag]
-
-    # get the start tiem of the first note
-    if len(onsets_frame) > 0:
-        first_onset_time = onsets_frame[0]
-
-        frames = librosa.time_to_frames(first_onset_time,sr=sr)
-
-        # create a new array that only contains the pitch data from the onset onwards
-        note_pitches = confident_f0[frames:]
-
-        # calculate the final representative pitch for the note
-        final_pitch = np.median(note_pitches)
-        final_note = librosa.hz_to_note(final_pitch)
-        print(f"Final detected pitch (median): {final_pitch} Hz")
-        print(f'frames: {frames}')
-        print(f'note_pitches: {note_pitches}')
-        print(f'final note: {final_note}')
+    detected_notes = get_detected_notes(onsets_frame,f0,voiced_flag)
 
 
+    print(detected_notes)
     
     # visualize(y, sr, f0, onsets)
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,24 @@ import librosa
 import numpy as np
 import matplotlib.pyplot as plt
 
+guitar_notes = {
+    'E2': 82.41,
+    'A2': 110,
+    'D3': 146.83,
+    'G3': 196,
+    'B3': 246.94,
+    'E4': 329.63
+}
+
+expected_ranges = {
+    'E2': (75, 90),
+    'A2': (100, 120),
+    'D3': (140, 160),
+    'G3': (190, 210),
+    'B3': (240, 260),
+    'E4': (320, 340)
+}
+
 def visualize(y, sr, f0, onsets):
     fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(10, 6))
 
@@ -30,3 +48,56 @@ def visualize(y, sr, f0, onsets):
 
     plt.tight_layout()
     plt.show()
+
+SAMPLE_RATE = 44100
+
+def load_audio_file(filename):
+    return librosa.load(filename)
+
+def get_pitch(y,sr):
+    
+    fmin = librosa.note_to_hz('E2')
+    fmax = librosa.note_to_hz('C7')
+    
+    f0, voiced_flag, voiced_prob = librosa.pyin(y, fmin=fmin, fmax=fmax, sr=sr)
+    return f0, voiced_flag
+
+def onset_detect(y,sr):
+    return librosa.onset.onset_detect(y=y, sr=sr, units='time')
+
+def get_detected_notes(onsets_frame, f0, voiced_flag):
+    detected_notes = []
+    # get the start tiem of the first note
+    if len(onsets_frame) > 0:
+
+        for i in range(len(onsets_frame)):
+
+            onset_time = onsets_frame[i]
+
+            start_frame = librosa.time_to_frames(onset_time,sr=sr)
+
+            if i < len(onsets_frame) - 1:
+                end_time = onsets_frame[i+1]
+            else:
+                end_time = librosa.get_duration(y=y,sr=sr)
+            end_frame = librosa.time_to_frames(end_time,sr=sr)
+
+
+            f0_segment = f0[start_frame:end_frame]
+            voiced_flag_segment = voiced_flag[start_frame:end_frame]
+
+            # create a new array with only the pitches we are confident about 
+            confident_f0 = f0_segment[voiced_flag_segment]
+
+
+            # calculate the final representative pitch for the note
+            final_pitch = np.median(confident_f0)
+            print(f'note_pitches: {final_pitch}')
+            final_note = librosa.hz_to_note(final_pitch)
+            # print(f"Final detected pitch (median): {final_pitch} Hz")
+            # print(f'frames: {frames}')
+            
+            print(f'final note: {final_note}')
+            detected_notes.append(final_note)
+
+    return detected_notes
